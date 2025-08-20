2bf162ee87ebc9d78a6b39aecff05ef6467e65be
Renato <36175588+renatoalvesjr@users.noreply.github.com>
2025-08-20 14:57:04 -0300

    config: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Oinkvest API')
+    .setDescription('Guia de rotas da API Oinkvest')
+    .setContact('Oinkvest','https://oinkvest.com.br','oinkvest@gmail.com')
+    .setOpenAPIVersion('3.1.0')
+    .setVersion('0.1.0')
+    .build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('swagger', app, documentFactory());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
