042ab4dc00bd4c7740c584b1ac26b2b6e2538871
renatoalvesjr <grangamin@gmail.com>
2025-08-26 20:47:44 -0300

    fix: erros eslint

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,13 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('Oinkvest API')
     .setDescription('Guia de rotas da API Oinkvest')
-    .setContact('Oinkvest','https://oinkvest.com.br','oinkvest@gmail.com')
+    .setContact('Oinkvest', 'https://oinkvest.com.br', 'oinkvest@gmail.com')
     .setOpenAPIVersion('3.1.0')
     .setVersion('0.1.0')
     .build();
-    const documentFactory = () => SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('swagger', app, documentFactory());
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory());
 
   await app.listen(process.env.PORT ?? 3001);
 }
-bootstrap();
+void bootstrap();
