c84885923708f86f7347fd740c1d1b4a9c694989
MrTch0o <waguinhotcho@Gmail.com>
2025-09-21 22:10:23 -0300

    fix module app

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { EmailModule } from './email/email.module';
 import { ConfigModule } from '@nestjs/config';
+import { BinanceModule } from './binance/binance.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { ConfigModule } from '@nestjs/config';
     UserModule,
     AuthModule,
     EmailModule,
+    BinanceModule,
   ],
   controllers: [AppController],
   providers: [AppService],
