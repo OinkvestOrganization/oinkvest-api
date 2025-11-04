c9523fc0d76188f935542ec3bf4d3aa51b2ba51b
maraeliza <marasampaioslm375@gmail.com>
2025-11-03 21:00:26 -0300

    retirada de comentario pra envio de email

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,6 @@
 import { UserService } from '@/user/user.service';
 import {
   ConflictException,
-  // ConflictException,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
@@ -9,7 +8,7 @@ import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { CreateUserDto } from '@/user/dto/create-user.dto';
 import { EmailService } from '@/email/email.service';
-// import { randomUUID } from 'crypto';
+import { randomUUID } from 'crypto';
 import { PrismaService } from '@/prisma/prisma.service';
 
 @Injectable()
@@ -22,24 +21,23 @@ export class AuthService {
   ) {}
 
   async register(createUserDto: CreateUserDto) {
-    await this.usersService.createUser(createUserDto);
-    // const user = await this.usersService.createUser(createUserDto);
-    // const token = randomUUID();
-    // const expiresIn = new Date(Date.now() + 1000 * 60 * 60 * 24);
+    const user = await this.usersService.createUser(createUserDto);
+    const token = randomUUID();
+    const expiresIn = new Date(Date.now() + 1000 * 60 * 60 * 24);
 
-    // const verificationToken = await this.prisma.verificationToken.create({
-    //   data: {
-    //     token: token,
-    //     expires: expiresIn,
-    //     userId: user.id,
-    //   },
-    // });
+    const verificationToken = await this.prisma.verificationToken.create({
+      data: {
+        token: token,
+        expires: expiresIn,
+        userId: user.id,
+      },
+    });
 
-    // await this.emailService.sendVerificationEmail(
-    //   user.email,
-    //   verificationToken.token,
-    //   user.nome,
-    // );
+    await this.emailService.sendVerificationEmail(
+      user.email,
+      verificationToken.token,
+      user.nome,
+    );
     return {
       message:
         'Cadastro realizado com sucesso. Verifique seu e-mail para ativar sua conta.',
@@ -62,7 +60,7 @@ export class AuthService {
 
     if (!user.emailVerificado) {
       throw new ConflictException(
-        'Conta ainda não verificada. Por favor verifique seu e-mail.',
+        'Conta ainda não verificada. Por favor, verifique seu e-mail.',
       );
     }
 
