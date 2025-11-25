566096d4792278fe6bdf43b4dbc01b86e529a773
realvesjr <grangamin@gmail.com>
2025-11-25 17:23:21 -0300

    feat: rota de esqueci minha senha e envio de email

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { UserService } from '@/user/user.service';
 import {
   ConflictException,
   Injectable,
+  Logger,
   UnauthorizedException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
@@ -10,9 +11,11 @@ import { CreateUserDto } from '@/user/dto/create-user.dto';
 import { EmailService } from '@/email/email.service';
 import { randomUUID } from 'crypto';
 import { PrismaService } from '@/prisma/prisma.service';
+import { ResetPasswordDto } from './dto/reset-password.dot';
 
 @Injectable()
 export class AuthService {
+  logger = new Logger(AuthService.name);
   constructor(
     private usersService: UserService,
     private jwtService: JwtService,
@@ -86,5 +89,61 @@ export class AuthService {
     }
     await this.usersService.activateUser(record.userId);
     return { message: 'Email verificado com sucesso.' };
+  }
+
+  async forgotPassword(email: string) {
+    const user = await this.usersService.findByEmail(email);
+    if (!user) {
+      this.logger.error('User not found');
+      throw new UnauthorizedException('Usuário não encontrado.');
+    }
+    this.logger.debug('User found' + user);
+    const token = randomUUID();
+    const expiresIn = new Date(Date.now() + 1000 * 60 * 60 * 24);
+
+    this.logger.debug(`Token created: ${token}`);
+    this.logger.debug(`Expires in: ${expiresIn}`);
+
+    await this.prisma.user.update({
+      where: { id: user.id },
+      data: {
+        passwordResetToken: token,
+        passwordResetExpires: expiresIn,
+      },
+    });
+
+    this.logger.debug('Token hashed, sending email...');
+    this.emailService.sendPasswordResetEmail(user.email, token);
+    return { message: 'Email de redefinição de senha enviado com sucesso.' };
+  }
+
+  async resetPassword(resetPassword: ResetPasswordDto) {
+    const { token, newPassword } = resetPassword;
+
+    const user = await this.prisma.user.findFirst({
+      where: {
+        passwordResetToken: token,
+        passwordResetExpires: {
+          gte: new Date(),
+        },
+      },
+    });
+
+    if (!user) {
+      this.logger.error('Token inválido');
+      return;
+    }
+
+    const salt = 10;
+    const hashedPassword = await bcrypt.hash(newPassword, salt);
+    await this.prisma.user.update({
+      where: { id: user.id },
+      data: {
+        senha: hashedPassword,
+        passwordResetToken: null,
+        passwordResetExpires: null,
+      },
+    });
+    return { message: 'Senha redefinida com sucesso.' };
   }
 }
--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -13,7 +13,7 @@ export class EmailService {
         <div style="max-width:600px;margin:0 auto;background:#fff;padding:50px;border-radius:8px;font-family:Arial,sans-serif;line-height:1.5;color:#333;text-align:center">
           <h1 style="color:#EAB308;font-size:40px;margin:0 0 12px 0;">OINKVEST</h1>
           <h2 style="color:#000;font-size:28px;margin:0 0 20px 0;">Seja muito bem vindo ${nome}. Para prosseguir, verifique seu email</h2>
-          
+      
           <p style="margin:0 0 20px 0;font-size:16px;color:#555;">
             Confirme que deseja usar esse endereço de e-mail para sua conta Oinkvest. 
             Assim que terminar, você poderá começar a investir!
@@ -34,6 +34,37 @@ export class EmailService {
 
     `;
 
+    return await this.mailer.sendMail({
+      to,
+      subject,
+      html,
+    });
+  }
+
+  async sendPasswordResetEmail(to: string, token: string) {
+    const resetUrl = `${process.env.APP_URL ?? 'http://localhost:3000'}/reset-password?token=${encodeURIComponent(token)}`;
+    const subject = 'Redefinição de senha';
+    const html = `
+    <div style="width:100%;background:#f4f4f5; margin:auto">
+      <div style="max-width:600px;margin:0 auto;background:#fff;padding:50px;border-radius:8px;font-family:Arial,sans-serif;line-height:1.5;color:#333;text-align:center">
+        <h1 style="color:#EAB308;font-size:40px;margin:0 0 12px 0;">OINKVEST</h1>
+        <h2 style="color:#000;font-size:28px;margin:0 0 20px 0;">Pedido de redefinição de senha</h2>
+        <p style="margin:0 0 20px 0;font-size:16px;color:#555;">
+          Para prosseguir com o processo de redifinição de senha, clique no botão abaixo.
+        </p>
+
+        <a href="${resetUrl}" 
+            style="display:inline-block;background:#7C3AED;color:#fff;padding:14px 24px;
+                    border-radius:8px;text-decoration:none;font-size:14px;font-weight:bold;width:auto">
+            REDEFINIR MINHA SENHA
+        </a>
+        <p style="margin:24px 0 8px 0;font-size:16px;color:#555;">Ou cole este link no seu navegador</p>
+        <a href="${resetUrl}" style="color:#2563eb;font-size:14px;text-decoration:none;">
+          ${resetUrl}
+        </a>
+      </div>
+    </div>
+          `;
     return await this.mailer.sendMail({
       to,
       subject,
